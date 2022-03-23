38666ef7453d5ef1b589c3b4b2746786c54439bc
Max Hilken <hilkenmc@gmail.com>
2022-03-23 19:30:31 -0400

    Employee test updated/commented out

--- src/test/java/com/revature/maxtermind/modelTest/EmployeeTest.java
+++ src/test/java/com/revature/maxtermind/modelTest/EmployeeTest.java
@@ -1,60 +1,77 @@
 package com.revature.maxtermind.modelTest;
 
+import com.revature.maxtermind.model.Application;
 import com.revature.maxtermind.model.Employee;
+import com.revature.maxtermind.model.Position;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeTest {
-//    Employee employee = new Employee(0, "password123", "Max", "Hilky", "example@example.com",
-//            1234567890, "espn.com", LocalDate.parse("2020-01-08"), position,
+//
+//
+//    private List<Employee> employees = new ArrayList<>();
+//    private Position position = new Position();
+//    private Employee john = new Employee();
+//
+//    Employee employee =
+//            new Employee(0, "password123", "Max", "Hilky", "example@example.com", 1234567890, "espn.com", LocalDate.parse("2020-01-08"),
+//            new Position(1, "john", BigDecimal.valueOf(39000), true, john, new ArrayList<>()),
+//            new ArrayList<>(),new ArrayList<>()
 //            );
 //
+//new Position(1, "John", 390000, true))
 //
+//        position.setApplications(new ArrayList<>());
+//        position.setId(1);
+//        position.setManager(john);
+//        position.setName("Administration Level 1");
+//        position.setSalary(BigDecimal.valueOf(39000));
 //
-//            max.setId(0);
-//        max.setPassword("password123");
-//        max.setFirstName("Max");
-//        max.setLastName("Hilky");
-//        max.setEmail("example@example.com");
-//        max.setPhoneNumber(1234567890);
-//        max.setPhoto("espn.com");
-//        max.setStartDate(LocalDate.parse("2020-01-08")); // Unsure of how to pass in Object Date
-//        max.setPosition(position);
-//        max.setNotifications(new ArrayList<>());
-//        max.setApplications(new ArrayList<>());
-//    );
-//
-//    @Test
-//    void setID_test(){
-//        employee.setId(0);
-//    }
-//
-//    @Test
-//    void setPassword_test(){
-//        employee.setId("Max");
-//    }
-//
-//    @Test
-//    void setID_test(){
-//        employee.setId(0);
-//    }
-//
-//    @Test
-//    void setID_test(){
-//        employee.setId(0);
-//    }
-//
-//    @Test
-//    void setID_test(){
-//        employee.setId(0);
-//    }
-//
-//    @Test
-//    void setID_test(){
-//        employee.setId(0);
-//    }
-//
+//        john.setPassword("123password");
+//        john.setFirstName("John");
+//        john.setLastName("Wayne");
+//        john.setEmail("another@example.com");
+//        john.setPhoneNumber(1234567809);
+//        john.setPhoto("apple.com");
+//        john.setStartDate(LocalDate.parse("2020-01-08")); // Unsure of how to pass in Object Date
+//        john.setPosition(position);// Unsure of how to pass in object Position
+//        john.setNotifications(new ArrayList<>());
+//        john.setApplications(new ArrayList<>());
+////    );
+////
+////    @Test
+////    void setID_test(){
+////        employee.setId(0);
+////    }
+////
+////    @Test
+////    void setPassword_test(){
+////        employee.setId("Max");
+////    }
+////
+////    @Test
+////    void setID_test(){
+////        employee.setId(0);
+////    }
+////
+////    @Test
+////    void setID_test(){
+////        employee.setId(0);
+////    }
+////
+////    @Test
+////    void setID_test(){
+////        employee.setId(0);
+////    }
+////
+////    @Test
+////    void setID_test(){
+////        employee.setId(0);
+////    }
+
 
 }
